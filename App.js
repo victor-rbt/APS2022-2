e94aed83afa2f85ae9bf840975a4f69ee87b9908
Victor Alves <victor.rob237@gmail.com>
2022-11-16 02:07:41 -0300

    ajustes gerais

--- App.js
+++ App.js
@@ -1,227 +1,187 @@
-import { Alert, StyleSheet, Button, Text, TextInput, View, Dimensions } from 'react-native';
-import React, { useState, useEffect } from "react";
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import {Alert, StyleSheet, Text, View, Dimensions, Image, StatusBar, TextInput} from 'react-native';
+import React, {useState, useEffect} from "react";
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 import * as Location from "expo-location";
+import enviroment from './enviroment/enviroment.json';
+import rodizio from './rodizio'
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen'; //to keep it responsive
 
 export default function App() {
-  let [posicao, setPosicao] = useState();
-
-  let [origem, setOrigem] = useState(null);
-  let [destino, setDestino] = useState(null);
+  let [posicao, setPosicao] = useState(null);
+  let [destino, setDestino] = useState();
 
   useEffect(() => {
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+      let {status} = await Location.requestForegroundPermissionsAsync();
 
-      if (status !== "granted") {
+      if (status !== "granted"){
         Alert.alert("Ops!", "Não foi possível acessar localização atual, setamos a localização padrão.");
         setPosicao({
           latitude: -23.51258725474799,
           longitude: -46.60913812329917,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        })
-      }
+          latitudeDelta: 0.00930,
+          longitudeDelta: 0.00300,
+        });
+      };
 
-      let { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      let {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
       setPosicao({
         latitude: latitude,
         longitude: longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitudeDelta: 0.00930,
+        longitudeDelta: 0.00300,
       });
     })()
   }, []);
 
+  function obterRodizio(){
+    let dataHoje = new Date();
+    return rodizio.rodizios[dataHoje.getDay()];
+  }
+
+  function obterDestino(){
+    return destino ? destino : {latitude: 0, longitude: 0};
+  }
+
   return (
     <View style={styles.container}>
-
       <GooglePlacesAutocomplete
-        placeholder='Enter Location'
-        minLength={2}
-        autoFocus={false}
+        placeholder='Destino..'
+        minLength={1}
+        suppressDefaultStyles={true}
         returnKeyType={'default'}
         fetchDetails={true}
-        styles={
-          {
-            predefinedPlacesDescription: {
-              color: '#1faadb',
+        styles={{
+          listView: {
+            position: 'absolute',
+            backgroundColor: '#FFF',
+            zIndex: 10,
+            marginTop: hp('7%')
+          },
+        
+          separator: {
+            height: StyleSheet.hairlineWidth,
+            backgroundColor: '#c8c7cc',
+          },
+          predefinedPlacesDescription: {
+            color: '#afaadb'
+          },
+          row: {
+            // backgroundColor: '#FFFFFF',
+            padding: 13,
+            minHeight: 44,
+            flexDirection: 'row',
+          },
+          loader: {
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            height: 20,
+          },
+          textInputContainer: {
+            width: Dimensions.get('window').width - 20,
+            marginBottom: 12,
+            backgroundColor: "white",
+            elevation: 4,
+            height: 45,
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            fontSize: 15,
+            marginTop: 12
+          },
+          textInput: {
+            width: "100%",
+            backgroundColor: '#FFFFFF',
+            height: 45,
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            fontSize: 15
             },
-            textInputContainer: {
-              position: 'absolute',
-              width: "97%",
-              marginLeft: "1.5%",
-              backgroundColor: "white",
-              elevation: 4,
-              padding: 0,
-              top: 50,
-              height: 44,
-              borderRadius: 5,
-              paddingHorizontal: 10,
-              fontSize: 15,
-
-            },
-            textInput: {
-              backgroundColor: '#FFFFFF',
-              height: 44,
-              borderRadius: 5,
-              paddingHorizontal: 10,
-              fontSize: 15,
-
-            }
-          }
-        }
-        onPress={(data, details = null) => {
-          setOrigem({
-            latitude: details.geometry.location.lat,
-            longitude: details.geometry.location.lng,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          });
-        }}
-        query={{
-          key: '',
-          language: 'pt-BR',
-        }}
-
-      />
-
-      <GooglePlacesAutocomplete
-        placeholder='Enter Location'
-        minLength={2}
-        autoFocus={false}
-        returnKeyType={'default'}
-        fetchDetails={true}
-        styles={
-          {
-            predefinedPlacesDescription: {
-              color: '#1faadb',
-            },
-            textInputContainer: {
-              position: 'absolute',
-              width: "97%",
-              marginLeft: "1.5%",
-              backgroundColor: "white",
-              elevation: 4,
-              padding: 0,
-              top: 100,
-              height: 44,
-              borderRadius: 5,
-              paddingHorizontal: 10,
-              fontSize: 15,
-
-            },
-            textInput: {
-              backgroundColor: '#FFFFFF',
-              height: 44,
-              borderRadius: 5,
-              paddingHorizontal: 10,
-              fontSize: 15,
-
-            }
           }
         }
         onPress={(data, details = null) => {
           setDestino({
             latitude: details.geometry.location.lat,
-            longitude: details.geometry.location.lng,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+            longitude: details.geometry.location.lng
           });
         }}
         query={{
-          key: '',
+          key: enviroment.apiKey,
           language: 'pt-BR',
-        }}
-
-      />
-
-      <MapView provider={PROVIDER_GOOGLE} style={styles.map} region={posicao} showsTraffic={true}>
-
+        }} />
+        <MapView
+          style={styles.mapContainer}
+          provider={PROVIDER_GOOGLE}
+          region={posicao}
+          showsTraffic={true}
+          showsUserLocation={true}>
         <MapViewDirections
-          origin={origem}
+          origin={posicao}
           destination={destino}
-          apikey={''}
+          apikey={enviroment.apiKey}
+          strokeWidth={8}
+          strokeColor={'cyan'}
         />
-
         <Marker
-          coordinate={{
-            latitude: -23.51258725474799,
-            longitude: -46.60913812329917
-          }}
-          title={"title"}
-          description={"description"}
-        />
-
-        <Marker
-          coordinate={{
-            latitude: -23.51258725474899,
-            longitude: -46.60913812339917
-          }}
-          title={"title"}
-          description={"description"}
-        />
+          coordinate={obterDestino()}
+          title={"Seu destino."} />
       </MapView>
-
+      <Text style={{marginBottom: 40, marginTop: 22}}>Rodízio hoje: {obterRodizio()}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    zIndex: 1,
+    flexDirection: 'column',
     flex: 1,
-  },
-  textInputContainer: {
-    position: 'absolute',
-    width: "90%",
-    backgroundColor: "white",
-    elevation: 4,
-    padding: 0,
-    top: 0,
-    height: 44,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    fontSize: 15,
-
-  },
-  textInput: {
-    backgroundColor: '#FFFFFF',
-    height: 44,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    fontSize: 15,
-
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  poweredContainer: {
-    justifyContent: 'flex-end',
     alignItems: 'center',
-    borderBottomRightRadius: 5,
-    borderBottomLeftRadius: 5,
-    borderColor: '#c8c7cc',
-    borderTopWidth: 0.5,
+    justifyContent: 'flex-start',
+    paddingTop: StatusBar.currentHeight,
   },
-  powered: {},
-  listView: {},
-  row: {
-    backgroundColor: '#FFFFFF',
-    padding: 13,
-    height: 44,
-    flexDirection: 'row',
+  mapContainer: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height - 150
   },
-  separator: {
-    height: 0.5,
-    backgroundColor: '#c8c7cc',
+  inputContainer:{
+    flexDirection: 'column',
+    width: 350,
   },
-  description: {},
-  loader: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    height: 20,
-  },
+  // poweredContainer: {
+  //   justifyContent: 'flex-end',
+  //   alignItems: 'center',
+  //   borderBottomRightRadius: 5,
+  //   borderBottomLeftRadius: 5,
+  //   borderColor: '#c8c7cc',
+  //   borderTopWidth: 0.5,
+  //   top: 700,
+  //   fontSize: 18
+  // },
+  // powered: {
+
+  // },
+  // listView: {
+
+  // },
+  // row: {
+  //   backgroundColor: '#FFFFFF',
+  //   padding: 13,
+  //   height: 44,
+  //   flexDirection: 'row',
+  //   fontSize: 18
+  // },
+  // separator: {
+  //   height: 0.5,
+  //   backgroundColor: '#c8c7cc',
+  // },
+  // description: {
+
+  // },
+  // loader: {
+  //   flexDirection: 'row',
+  //   justifyContent: 'flex-end',
+  //   height: 20,
+  // },
 });
